ee63c69a664e7bd7fa7c7b34f6a3a787b77efafd
yDevLuisDias <luiscosta.official@gmail.com>
2025-07-22 23:04:39 -0300

    add leitor de arquivos = E

--- src/main/java/com/etl_basic/service/ExtractService.java
+++ src/main/java/com/etl_basic/service/ExtractService.java
@@ -1,10 +1,20 @@
 package com.etl_basic.service;
 
+import com.etl_basic.model.CsvEntity;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ExtractService {
+
+    TransformService transformService;
+
+    public ExtractService(TransformService transformService) {
+        this.transformService = transformService;
+    }
 
     List<String> csvColumns = new ArrayList<>();
     List<String> jsonColumns = new ArrayList<>();
@@ -13,8 +23,30 @@ public class ExtractService {
     public ExtractService() {
     }
 
-    public void inputPathCsv(String path){
+    public CsvEntity inputPathCsv(String path){
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 
+            String line = br.readLine();
+            var header = line.split(",");
+
+            while((line = br.readLine()) != null){
+
+                var data = line.split(",");
+
+                csvColumns.add(data[0]);
+                csvColumns.add(data[1]);
+                csvColumns.add(data[2]);
+
+                if (csvColumns == null){
+                    System.out.println("Vazio");
+                }
+            }
+
+        }catch(IOException e){
+            System.out.println("ERROR : " + e.getMessage());
+        }
+
+        return new CsvEntity();
     }
 
     public void inputPathJson(String path){
