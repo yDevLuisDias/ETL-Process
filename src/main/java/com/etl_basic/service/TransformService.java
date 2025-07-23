ad5436c4a3bda3fa3a08d148c3365ee22585ca95
yDevLuisDias <luiscosta.official@gmail.com>
2025-07-22 23:05:39 -0300

    testando return do extractService

--- src/main/java/com/etl_basic/service/TransformService.java
+++ src/main/java/com/etl_basic/service/TransformService.java
@@ -1,4 +1,15 @@
 package com.etl_basic.service;
 
 public class TransformService {
+    ExtractService extractService;
+
+    public TransformService() {
+        this.extractService = new ExtractService();
+    }
+
+    public void Test(){
+        extractService.inputPathCsv("./src/main/java/com/etl_basic/data/file.csv");
+
+        System.out.println(extractService.csvColumns);
+    }
 }
